13aa1d3ba3484704b108fd3c8a91a30fc8a2ce31
fares boutriga <boutrigafares@gmail.com>
2023-08-01 16:11:25 +0100

    fix flies

--- Client/pages/Home.jsx
+++ Client/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {  View,Text } from 'react-native'
 
 function Home() {
   return (
-    <div>
-      
-    </div>
+    <View>
+     <Text>home</Text>
+    </View>
   )
 }
 
