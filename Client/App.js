cbb47b34db365b8efbb0297ddce49095e442dfec
fares boutriga <boutrigafares@gmail.com>
2023-08-01 18:07:51 +0100

    fix flies1

--- Client/App.js
+++ Client/App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './pages/Home';
+import Home from './pages/ClientPages/Home';
+import NavBar from './components/NavBar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello fares </Text>
-      <Text>hello fares  dgsd</Text>
       <StatusBar style="auto" />
+      <Text>hello fares </Text>
+      <Text>hello fares  </Text> 
     
     <Home/>
-
+    <NavBar/>
     </View>
   );
 }
@@ -17,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  
     alignItems: 'center',
     justifyContent: 'center',
   },
