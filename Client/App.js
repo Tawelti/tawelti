3b7cca33e9bb75df8d977951f916681682497c49
fares boutriga <boutrigafares@gmail.com>
2023-08-04 11:38:28 +0100

    fix the app.js

---
+++ Client/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './pages/ClientPages/Home';
+import NavBar from './components/NavBar';
+import Comments from './pages/ClientPages/Comments';
+import Profil from './components/SellerComponents/profil';
+import Ratings from './components/ClientComponent/Ratings';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      {/* <Text>hello fares </Text>
+      <Text>hello fares   </Text>  */}
+     {/* <Home/> */}
+     <Ratings/>
+     {/* <Comments/> */}
+   {/* <Profil/> */}
+   
+    <NavBar/> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
