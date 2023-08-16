b240d6c471366a9d8628c3f469de77bdd1d085c0
samarzribi <samarzribi014@gmail.com>
2023-08-16 14:30:57 +0100

    working on payment method

--- Server/controllers/FavoriteController.js
+++ Server/controllers/FavoriteController.js
@@ -2,6 +2,7 @@ const {Favorite}= require('../database/models/Favorite')
 
 module.exports= {
  createFavorite : (req , res) => {
+   const { clientId, placeId } = req.params;
     Favorite.create({
       Client_id: clientId,
        Place_id: placeId,
@@ -13,8 +14,25 @@ module.exports= {
       console.error(error);
       res.status(500).json({ error });
     });
-
  },
+
+ getAllFavoritesByClientId: (req, res) => {
+   const { clientId , placeId} = req.params;
+
+
+   Favorite.findAll({
+     where: {
+       Client_id: clientId,
+     },
+   })
+   .then((favorites) => {
+     res.status(200).json(favorites);
+   })
+   .catch((error) => {
+     console.error(error);
+     res.status(500).json({ error });
+   });
+},
 
  
 }
