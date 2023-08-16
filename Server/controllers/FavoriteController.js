0079d76a817928d8c71f8a9c815fc156f42475af
samarzribi <samarzribi014@gmail.com>
2023-08-16 13:25:22 +0100

    fixing payment method

--- Server/controllers/FavoriteController.js
+++ Server/controllers/FavoriteController.js
@@ -2,6 +2,19 @@ const {Favorite}= require('../database/models/Favorite')
 
 module.exports= {
  createFavorite : (req , res) => {
-    Favorite.create({})
- }
+    Favorite.create({
+      Client_id: clientId,
+       Place_id: placeId,
+    })
+    .then((respense) => {
+      res.status(200).json(respense);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error });
+    });
+
+ },
+
+ 
 }
